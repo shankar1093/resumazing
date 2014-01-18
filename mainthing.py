484a51e824433ceb0b95b5f79be06d15930be46b
Sameer Ansari <elucidation@gmail.com>
2014-01-18 03:39:16 -0500

    keyword and entity extraction working

--- mainthing.py
+++ mainthing.py
@@ -6,6 +6,9 @@ from pdfminer.pdfparser import PDFParser
 import requests
 import xml.etree.ElementTree as ET
 
+# Secret Key
+API_KEY = "bae55da9dafaa2c7b9e8d78e678d69c4ce210ddf"
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = '/path/to/the/uploads'
@@ -16,18 +19,36 @@ def parseRankedKeywords(xml_str):
     words = {}
     root = ET.fromstring(xml_str)
     for keyword in root.iter('keyword'):
-        name = keyword[0].text
+        text = keyword[0].text
         relevance = keyword[1].text
-        words[name] = relevance
+        words[text] = relevance
     return words
+
+def parseRankedEntities(xml_str):
+    entities = []
+    root = ET.fromstring(xml_str)
+    for entity in root.iter('entity'):
+        _type = entity[0].text
+        relevance = entity[1].text
+        count = entity[2].text
+        text = entity[3].text
+        entities.append( (_type, relevance, count, text) )
+    return entities
+
 
 @app.route("/")
 def hello():
+    
     return "Hello World!"
 
-@app.route("/test/<msg>")
-def test(msg=None):
-    url = "http://access.alchemyapi.com/calls/text/TextGetRankedKeywords?apikey=bae55da9dafaa2c7b9e8d78e678d69c4ce210ddf&text=%s" % msg
+@app.route("/test")
+@app.route("/test/")
+@app.route("/test/<query_type>")
+def test(query_type="TextGetRankedKeywords"):
+    msg = request.args.get('msg')
+    if not msg:
+        return "Need to add ?msg= parameter"
+    url = "http://access.alchemyapi.com/calls/text/%s?apikey=%s&text=%s" % (query_type, API_KEY, msg)
     # url = "http://127.0.0.1"
     try:
         r = requests.get(url)
@@ -37,7 +58,19 @@ def test(msg=None):
     except requests.exceptions.ConnectionError as e:
         return "Connection Error"
     
-    
+@app.route("/entity")
+def getEntities(query_type="TextGetRankedNamedEntities"):
+    msg = request.args.get('msg')
+    if not msg:
+        return "Need to provide text in a 'msg' parameter"
+    url = "http://access.alchemyapi.com/calls/text/%s?apikey=%s&text=%s" % (query_type, API_KEY, msg)
+    try:
+        r = requests.get(url)
+        entities = parseRankedEntities(r.text)
+        # entities = ( (1,2,3,4), ('blah',3,6,3), (69,43,23,12) )
+        return render_template('entities.html', original=msg, entities=entities)
+    except requests.exceptions.ConnectionError as e:
+        return "Connection Error"
 
 if __name__ == "__main__":
     app.run(debug=True)
