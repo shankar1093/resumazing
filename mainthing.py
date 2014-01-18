8187855d277058def82858412db726d0f04b17a8
Sameer Ansari <elucidation@gmail.com>
2014-01-18 07:54:54 -0500

    moved to alchemyapi, added results page for comparing job listing to resume data

--- mainthing.py
+++ mainthing.py
@@ -9,6 +9,7 @@ import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import ParseError
 import sys
 import pyPdf
+import json
 
 from alchemyapi import AlchemyAPI
 alchemyapi = AlchemyAPI()
@@ -34,26 +35,6 @@ UPLOAD_FOLDER = '/home/sam/files'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def parseRankedKeywords(xml_str):
-    words = {}
-    root = ET.fromstring(xml_str)
-    for keyword in root.iter('keyword'):
-        text = keyword[0].text
-        relevance = keyword[1].text
-        words[text] = relevance
-    return words
-
-def parseRankedEntities(xml_str):
-    entities = []
-    root = ET.fromstring(xml_str)
-    for entity in root.iter('entity'):
-        _type = entity[0].text
-        relevance = entity[1].text
-        count = entity[2].text
-        text = entity[3].text
-        entities.append( (text, relevance, _type, count) )
-    return entities
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -61,7 +42,11 @@ def allowed_file(filename):
 
 ########################################3
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+@app.route('/test', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
@@ -89,10 +74,10 @@ def getKeywords(query_type="TextGetRankedKeywords"):
     if not msg:
         return "Need to add ?msg= parameter"
     url = "http://access.alchemyapi.com/calls/text/%s?apikey=%s&text=%s" % (query_type, API_KEY, msg)
-    # url = "http://127.0.0.1"
+
     try:
         r = requests.get(url)
-        words = parseRankedKeywords(r.text)
+        words = get_keywords_from_text(r.text)
         # words = {'boop':5,'abscd':6,'Alop':29}
         return render_template('keywords.html', original=msg, words=words)
     except requests.exceptions.ConnectionError as e:
@@ -106,13 +91,13 @@ def getEntities(query_type="TextGetRankedNamedEntities"):
     url = "http://access.alchemyapi.com/calls/text/%s?apikey=%s&text=%s" % (query_type, API_KEY, msg)
     try:
         r = requests.get(url)
-        print "----" 
-        print r.text
-        print "===="
         try:
-            entities = parseRankedEntities(r.text)
-            # entities = ( (1,2,3,4), ('blah',3,6,3), (69,43,23,12) )
-            return render_template('entities.html', original=msg, entities=entities)
+            entities = get_entities_from_text(r.text)
+            format = request.args.get('format')
+            if format == 'json':
+                return json.dumps(entities)
+            else:
+                return render_template('entities.html', original=msg, entities=entities)
         except ParseError as e:
             return r.text
     except requests.exceptions.ConnectionError as e:
@@ -126,15 +111,75 @@ def getEntitiesFromURL():
     data = get_entities_from_url(url)
     if not data:
         return "Error getting url data"
-    return render_template('entities.html', original=url, entities=data)
+
+    format = request.args.get('format')
+    if format == 'json':
+        return json.dumps(data)
+    else:
+        return render_template('entities.html', original=url, entities=data)
+
+def get_entities_from_text(text):
+    response = alchemyapi.entities('text',text, { 'sentiment':1 })
+    if response['status'] == 'OK':
+        return [(e['text'], e['relevance'], e['type']) for e in response['entities']]   
+    else:
+        print('Error in entity extraction call: ', response['statusInfo'])
+        return None
+
 
 def get_entities_from_url(url):
-	response = alchemyapi.entities('url',url, { 'sentiment':1 })
-	if response['status'] == 'OK':
-		return [(e['text'], e['relevance'], e['type']) for e in response['entities']]	
-	else:
-		print('Error in entity extraction call: ', response['statusInfo'])
-		return None
+    response = alchemyapi.entities('url',url, { 'sentiment':1 })
+    if response['status'] == 'OK':
+        return [(e['text'], e['relevance'], e['type']) for e in response['entities']]   
+    else:
+        print('Error in entity extraction call: ', response['statusInfo'])
+        return None
+
+def get_keywords_from_text(text):
+    response = alchemyapi.keywords('text',text, { 'sentiment':1 })
+    if response['status'] == 'OK':
+        return [(e['text'], e['relevance']) for e in response['keywords']]
+    else:
+        print('Error in keyword extraction call: ', response['statusInfo'])
+        return None
+
+def get_keywords_from_url(url):
+    response = alchemyapi.keywords('url',url, { 'sentiment':1 })
+    if response['status'] == 'OK':
+        return [(e['text'], e['relevance']) for e in response['keywords']]
+    else:
+        print('Error in keyword extraction call: ', response['statusInfo'])
+        return None
+
+@app.route("/query", methods=['POST'])
+def doQuery():
+    # Job Listing URL
+    job_url = request.form['url']
+    if not job_url:
+        return "Need url parameter"
+    job_entity_data = get_entities_from_url(job_url)
+    if not job_entity_data:
+        return "Error getting url data"
+
+    # Resume PDF file
+    resume_entity_data = ""
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+        # Save PDF file to directory
+        file.save(filepath)
+
+        # Get text from PDF file
+        resume_text = getPDFContent(filepath)
+
+        # Get Entity data from PDF text
+        resume_entity_data = get_entities_from_text(resume_text)
+
+    return render_template('results.html', job_entity_data=job_entity_data, 
+                                            resume_entity_data=resume_entity_data)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
