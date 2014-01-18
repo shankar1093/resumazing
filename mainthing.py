56e00cc1d5a89a53851494ca4cbcd4c6ceb30c41
Sameer Ansari <elucidation@gmail.com>
2014-01-18 05:05:48 -0500

    make external access

--- mainthing.py
+++ mainthing.py
@@ -79,4 +79,4 @@ def getEntities(query_type="TextGetRankedNamedEntities"):
         return "Connection Error"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
